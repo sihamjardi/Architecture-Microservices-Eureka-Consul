311999036fc4758cee839f4d22572bf9d926b34b
Your Name <sihamjardi2@gmail.com>
2026-01-11 03:06:37 +0100

    first commit

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -5,9 +5,11 @@ import com.example.client.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientApplication {
 
     public static void main(String[] args) {
